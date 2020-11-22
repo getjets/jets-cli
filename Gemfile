8a0a1b5315dcabd0e65b636cbb1cf5756658145b
Lauri Jutila <git@laurijutila.com>
2020-11-22 10:36:38 +0200

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in jets-cli.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
+gem "rspec", "~> 3.0"
+gem "jets-rubocop", path: "../jets-rubocop"
---
+++ spec/jets/cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jets::Cli do
+  it "has a version number" do
+    expect(Jets::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
