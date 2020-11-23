d23bf3849d0ed4b9177b418ff03c4e625c902f2c
Lauri Jutila <git@laurijutila.com>
2020-11-23 11:11:41 +0200

    Initial engine generator.

---
+++ lib/generators/jets/engine/engine_generator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Jets
+  module Generators
+    class EngineGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("templates", __dir__)
+
+      def create_engine_file
+        directory(".", "engines/#{name}")
+
+        chmod "engines/#{name}/bin/console", 0o755, verbose: false
+        chmod "engines/#{name}/bin/rails", 0o755, verbose: false
+      end
+    end
+  end
+end
